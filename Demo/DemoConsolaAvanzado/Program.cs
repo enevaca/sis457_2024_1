17fd74aa4948b3646aef3d4a84a45a6b4691a5c5
Noel Vaca <noel.vm.2501@gmail.com>
2024-03-16 22:17:12 -0400

    creación del proyecto demo consola avanzado, demo inmutabilidad

---
+++ Demo/DemoConsolaAvanzado/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoConsolaAvanzado;
+
+// Inmutabilidad
+Inmutable inmutable = new Inmutable(2002);
+// inmutable.year = 2003; no se puede modificar
+
+Console.WriteLine("Hello, World!");
--- Demo/DemoConsolaAvanzado/obj/Debug/net8.0/DemoConsolaAvanzado.AssemblyInfo.cs
+++ Demo/DemoConsolaAvanzado/obj/Debug/net8.0/DemoConsolaAvanzado.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DemoConsola")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DemoConsolaAvanzado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382b53f95e5c88204a66576edbf54302e4b74dad")]
-[assembly: System.Reflection.AssemblyProductAttribute("DemoConsola")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DemoConsola")]
+[assembly: System.Reflection.AssemblyProductAttribute("DemoConsolaAvanzado")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DemoConsolaAvanzado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
