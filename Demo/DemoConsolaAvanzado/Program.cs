c1dd800e872032ff67c7721046a76756ce7c3495
Noel Vaca <noel.vm.2501@gmail.com>
2024-03-18 22:06:25 -0400

    async/await, try/catch/finally, abstract, interface

--- Demo/DemoConsolaAvanzado/Program.cs
+++ Demo/DemoConsolaAvanzado/Program.cs
@@ -1,8 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DemoConsolaAvanzado;
 
+// Interfaces
+// IVehiculo vehiculo = new IVehiculo(); // no se puede instanciar
+Automovil automovil = new Automovil();
+automovil.acelerar(60);
+automovil.frenar();
+automovil.girar(15);
+
+// Clases Abstractas
+// FiguraGeometrica figuraGeometrica = new FiguraGeometrica(); // no se puede instanciar
+Cuadrado cuadrado = new Cuadrado(5);
+Console.WriteLine($"El área del cuadrado es: {cuadrado.area()}");
+Console.WriteLine($"El perímetro del cuadrado es: {cuadrado.perimetro()}");
+
+Rectangulo rectangulo = new Rectangulo(10, 7);
+Console.WriteLine($"El área del rectángulo es {rectangulo.area()}");
+Console.WriteLine($"El perímetro del rectángulo es {rectangulo.perimetro()}");
+
+// try/cath/finally
+int a, b, division;
+try
+{
+    Console.Write("Introduzca número 1: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Introduzca número 2: ");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    division = a / b;
+    Console.WriteLine($"El resultado es {division}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("No es posible la división por Cero. " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Excepción no controlada. " + ex.Message);
+}
+finally
+{
+    division = 0;
+    Console.WriteLine("Finalizado");
+}
+
+// Programación Asíncrona (async/await)
+testAsyncAwaitMethod();
+Console.WriteLine("Presione cualquier tecla para salir...");
+Console.ReadLine();
+
+static async void testAsyncAwaitMethod()
+{
+    await longRunningMethod();
+}
+
+static async Task<int> longRunningMethod()
+{
+    Console.WriteLine("Iniciando un método de larga duración...");
+    await Task.Delay(5000);
+    Console.WriteLine("Fin del método de larga duración");
+    return 1;
+}
+
 // Inmutabilidad
 Inmutable inmutable = new Inmutable(2002);
 // inmutable.year = 2003; no se puede modificar
-
-Console.WriteLine("Hello, World!");
--- Demo/DemoConsolaAvanzado/obj/Debug/net8.0/DemoConsolaAvanzado.AssemblyInfo.cs
+++ Demo/DemoConsolaAvanzado/obj/Debug/net8.0/DemoConsolaAvanzado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoConsolaAvanzado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382b53f95e5c88204a66576edbf54302e4b74dad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fd74aa4948b3646aef3d4a84a45a6b4691a5c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoConsolaAvanzado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoConsolaAvanzado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
